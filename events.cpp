12e2e98c0d6bc83de81a79c685af5c7ff84c3859
Tobias Tiederle <ttiederle@fimltd.org>
2016-06-14 12:43:14 +0100

    original source

---
+++ events.cpp
@@ -0,0 +1,40 @@
+#include "dsofinder.h"
+
+//THIS FILE CONTAINS BASIC INFORMATION ON EVENTS AND RELATED ITEMS
+//FOR DETAILED ITEM DEFINITIONS SEE items.cpp
+
+void DsoFinder::init_events()
+{
+    eventcount = 7;
+
+    events = new event[eventcount];
+    i=0;
+
+    events[0].caption = "None";
+    events[0].id = 0;
+    events[0].itemcount = 0;
+
+    events[1].caption = "Halloween 2013";
+    events[1].id = 1;
+    events[1].itemcount = 1;
+
+    events[2].caption = "Christmas 2013/14";
+    events[2].id = 2;
+    events[2].itemcount = 3;
+
+    events[3].caption = "Easter 2014";
+    events[3].id = 3;
+    events[3].itemcount = 4;
+
+    events[4].caption = "Soccer WM 2014";
+    events[4].id = 4;
+    events[4].itemcount = 8;
+
+    events[5].caption = "Halloween 2014";
+    events[5].id = 5;
+    events[5].itemcount = 1;
+
+    events[6].caption = "Easter 2015";
+    events[6].id = 6;
+    events[6].itemcount = 1;
+}
