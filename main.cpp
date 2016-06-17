bcac30a4d27e9c9bbf04e0ddd9aa73aab6ea7447
Tobias Tiederle <ttiederle@fimltd.org>
2016-06-17 13:52:18 +0100

    add license notice to files

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/*
+ *  This file is part of TSO Combofinder.
+ *
+ *  Copyright 2010-2014 by malkolm <tobid@gmx.ch>
+ *  Copyright 2016 by EmTeedee <emteedee@web.de>
+ *
+ *  TSO Combofinder is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  TSO Combofinder is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with TSO Combofinder.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QApplication>
 #include "tsofinder.h"
 
